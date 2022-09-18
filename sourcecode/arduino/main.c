4f88c82955c505d97549547743d335d8ace808b1
Yannick Lang <layaxx@gmx.de>
2022-09-18 13:41:58 +0200

    update arduino code

--- sourcecode/arduino/main.c
+++ sourcecode/arduino/main.c
@@ -1,11 +1,11 @@
+/* cSpell:disable */
 #define __AVR_ATmega328P__ // Suppress errors in VS Code
 
 #define FOSC 16000000UL // Clock Speed
-#define BAUD 19200		// Desired BAUD
+#define BAUD 250000		// Desired BAUD
 #define MYUBRR FOSC / 16 / BAUD - 1
 
 #include <stdio.h>
-
 #include <avr/io.h> // Contains all the I/O Register Macros
 #include <avr/interrupt.h>
 #include <util/delay.h> // Generates a Blocking Delay
@@ -35,7 +35,7 @@ volatile short dataReadyMPU = 0;
 volatile short dataReadyUS = 0;
 volatile uint16_t dataMPU;
 volatile uint16_t dataUS;
-long UStarget = 20000;
+volatile uint16_t dataUSPre;
 
 uint8_t data[2];
 
@@ -71,8 +71,8 @@ int __attribute__((OS_main)) main(void)
 	// set D2 as input
 	DDRD &= ~(1 << PORTD2);
 
-	// INT1 interrupt on falling edge and INT0 interrupt on rising edge
-	EICRA |= 0b1011;
+	// INT1 interrupt on logic level change INT0 interrupt on rising edge
+	EICRA |= 0b0111;
 	// ENABLE INT0 interrupt
 	EIMSK |= (1 << INT0);
 	// ENABLE INT0 interrupt
@@ -81,41 +81,27 @@ int __attribute__((OS_main)) main(void)
 	SREG |= (1 << SREG_I);
 
 	TCCR1B |= 0b010; // setup clock, use 8 prescaler
-	TIMSK1 |= 1;	 // enable overflow interrupt
+	// TIMSK1 |= 1;	 // enable overflow interrupt
 
-	uint16_t counter = 0;
-
-	short hasSentSomething = 0;
+	trigger();
 
 	while (1)
 	{
 		if (dataReadyMPU)
 		{
-			putString("GyroX: ");
-			// putDec(counter);
-			// putChar(' ');
+			putString("G: ");
 			I2C_read_registers(address, 67, data, 6);
 			putDec((data[0] << 8) | data[1]);
 			putString("\r\n");
 			dataReadyMPU = 0;
-			hasSentSomething = 1;
 		}
 		if (dataReadyUS)
 		{
-			putString("Ultrasound: ");
-			// putDec(counter);
-			// putChar(' ');
-			putDec(dataUS);
+			putString("U: ");
+			putDec(dataUS - dataUSPre);
 			putString("\r\n");
 			dataReadyUS = 0;
-			hasSentSomething = 1;
-		}
-
-		if (hasSentSomething)
-		{
-			// Only increment Counter if it was actually used
-			counter++;
-			hasSentSomething = 0;
+			trigger();
 		}
 	}
 	return 0;
@@ -126,28 +112,24 @@ ISR(INT0_vect)
 	// Read Gyro Data
 	dataReadyMPU = 1;
 }
-
-ISR(INT1_vect)
-{
-	dataUS = TCNT1;
-	dataReadyUS = 1;
-
-	TCNT1 |= 0b11111111111111111111111111111111111110;
-
-	// if (TCNT1 > UStarget)
-	// {
-	// 	// Missed target, trigger immediately
-	// 	TCNT1 |= 0b11111111111111110;
-	// }
-	// else
-	// {
-	// 	TCNT1 = 0b1111111111111111 - (UStarget - TCNT1); // Get to target
-	// }
-}
-
-ISR(TIMER1_OVF_vect)
+short justOverflowed = 0;
+void trigger()
 {
 	PORTD |= (1 << PORTD4);
-	_delay_us(20);
+	_delay_us(15);
 	PORTD &= ~(1 << PORTD4);
+	justOverflowed = 1;
+}
+ISR(INT1_vect)
+{
+	if (justOverflowed)
+	{
+		dataUSPre = TCNT1;
+		justOverflowed = 0;
+	}
+	else
+	{
+		dataUS = TCNT1;
+		dataReadyUS = 1;
+	}
 }
